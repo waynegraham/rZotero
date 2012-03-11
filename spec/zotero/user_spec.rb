ba079f80110e6e79b573c23e5a329b7508a31308
Wayne Graham <wayne.graham@virginia.edu>
2012-03-10 20:34:01 -0500

    spec tests for base, client, and user

--- spec/zotero/user_spec.rb
+++ spec/zotero/user_spec.rb
@@ -3,11 +3,25 @@ require 'helper'
 describe Zotero::User do
 
   describe "#==" do
+    
     it "should return true when ids and classes are equal" do
       user = Zotero::User.new('id' => 1)
       other = Zotero::User.new('id' => 1)
       (user == other).should be_true
     end
+
+    it "should return false when classes are not equal" do
+      user = Zotero::User.new('id' => 1)
+      other = Zotero::Client.new('id' => 1)
+      (user == other).should be_false
+    end
+  
+    it "should return false when ids are not equal" do
+      user = Zotero::User.new('id' => 1)
+      other = Zotero::User.new('id' => 2)
+      (user == other).should be_false
+    end
+
   end
 
 end
